5c3d5fb26bfb3c21c8b30d6384bbd2eac944360b
amkera <60901491+amkera@users.noreply.github.com>
2020-10-16 11:17:05 -0700

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,45 @@
 // Code ClickityClick Component Here
+//state is only for data that we know is going to change in the component,
+//and is visible in the UI
+
+//instead of directly modifying the state using this.state, we use this.setState()
+//this way we can tell React that a component state has changed
+//component will know to rerender
+
+//For example, let's say we have a component with a button, and a bit of text to indicate whether that button has been pressed yet:
+
+import React from 'react';
+
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+    //let's define the initial state
+    this.state = {
+      hasBeenClicked: false
+    };
+  }
+
+  handleClick = () => {
+    //To update our state, we use this.setState() and pass in an object.
+    //This object will get merged with the current state. //
+    //When the state has been updated, our component re-renders automatically.
+    this.setState({
+      hasBeenClicked: true
+    })
+    console.log(this.state.hasBeenClicked); //false
+    //() => console.log(this.state.hasBeenClicked)) //true
+  }
+
+  //The console output says false... but we just set it to true! What is this madness?
+  //State changes, however instant they might appear, happen asynchronously. If we want to access our new state after it has been updated, we can optionally add a callback as a second argument to the this.setState() function. This callback will fire once the state has been updated, ensuring that this.state is now the new, shiny updated state. In code:
+
+  render() {
+    return (
+      <div>
+        <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    )
+  }
+}
+export default ClickityClick;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClick from './components/ClickityClick'
 
 ReactDOM.render(
   <div>
-    Mount Components Here
+    <ClickityClick />
   </div>,
   document.getElementById('root')
 );
+
+
+// The spread operator syntax can be used in JavaScript to 'de-compose'
+// objects and arrays. When used on an object as we see above,
+// ...this.state.addressInfo returns all the keys and values from within
+//  that object. We're saying addressInfo should be equal to all the keys and
+//  values that make up addressInfo, and, in addition, there should be city
+//  key with the value New York City. If there is already a city key
+//  inside addressInfo, it will be overwritten. If it doesn't exist,
+//  it will be added.
+
+
+// It's important to note the difference between changes in state and changes in props. Changes in state and/or props will both trigger a re-render of our React component. However, changes in state can only happen internally due to components changing their own state. Thus, a component can trigger changes in its own state.
+//
+// A component cannot change its props. Changes in props can only happen externally, meaning the parent or grandparent component changes the values it passing down to its children.
+
+//so change in state or props will rerender
+//but changes in state only happen internally due to components changing their own state
+//but components cannnot change props
